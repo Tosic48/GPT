a0487d7c3509d60070a04045b290dbb1226823a4
Anton S <101250188+Tosic48@users.noreply.github.com>
2023-02-10 11:31:41 +0300

    Update open_api.py

--- open_api.py
+++ open_api.py
@@ -11,8 +11,8 @@ import telebot
 import openai
 
 
-bot = telebot.TeleBot("6023813534:AAE6i3zDhxSX_ZGs74dbQlMSqmpfAjLWmw0")
-openai.api_key = "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"
+bot = telebot.TeleBot("*")
+openai.api_key = "*"
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
