824cdc08b1ce06e576924b7303151bb0a027119b
“Anton_Mint” <antonsuhodolski@tut.by>
2023-02-11 11:05:53 +0300

    ch api

--- open_api.py
+++ open_api.py
@@ -1,15 +1,12 @@
 # Что такое Chat GPT - создаем Телеграм бот для общения с нейросетью OpenAI
-
 # Подписывайтесь, чтобы
 # быстрее узнавать о
 # новых уроках - https://t.me/isartem_bot
-
 # pip install pyTelegramBotAPI
 # pip install openai
 from config import api_ident
 import telebot
 import openai
-
 
 bot = telebot.TeleBot("6023813534:AAE6i3zDhxSX_ZGs74dbQlMSqmpfAjLWmw0")
 openai.api_key = api_ident
